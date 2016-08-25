0ec1293664eba1bc02d9e3c6792502ea495ffd77
Max <max.t.quinn@gmail.com>
2016-08-25 15:37:05 +1200

    added a mock page to submit data and connected to database

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ var app = express();
 var bodyParser = require('body-parser')
 
 var port = process.env.port || 3000;
+const MongoClient = require('mongodb').MongoClient
+
+var db
+//Set up and connect to mongo database
+
+MongoClient.connect('mongodb://dimethyltryptamine:Dmtmakeamandream1@ds021691.mlab.com:21691/staysavage-media-content', function (err, database) {
+    if (err) return console.log(err);
+    db = database;
+    //connect to server
+    app.listen(port, function () {
+        console.log('listening on port ' + port);
+    });
+})
 
 // Setup Views & View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -13,27 +26,41 @@ app.set('view engine', 'html');
 // Define ./public as static
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
     extended: true
 }));
 
-app.get('/', function(req, res) {
-    return res.render('index');
+//get handlers
+app.get('/', function (req, res) {
+    return res.render('index.html');
 });
 
-app.get('/feature', function(req, res) {
-    return res.render('feature');
+app.get('/feature', function (req, res) {
+    return res.render('feature.html');
 });
 
-app.get('/shop', function(req, res) {
-    return res.render('shop');
+app.get('/shop', function (req, res) {
+    return res.render('shop.html');
 });
 
-app.get('/crew', function(req, res) {
-    return res.render('crew');
+app.get('/crew', function (req, res) {
+    return res.render('crew.html');
 });
 
-app.listen(port, function() {
-    console.log('listening on port ' + port);
+app.get('/submitdata', function (req, res) {
+    db.collection('data').find().toArray(function(err, result) {
+        if (err) return console.log(err);
+        return res.render('submitdata.ejs', {data: result});
+    });
+});
+
+//post handlers
+app.post('/submitpost', function (req, res) {
+    db.collection('data').save(req.body, function(err, result) {
+        if (err) return console.leg(err);
+
+        console.log('saved to database');
+        res.redirect('/');
+    });
 });
