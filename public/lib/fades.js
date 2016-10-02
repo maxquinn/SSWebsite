d38ad234be2198cf124d1de9d675f0b33aeeac80
maxquinn <max.t.quinn@gmail.com>
2016-10-03 10:51:47 +1300

    added fade in for logo on scroll

---
+++ public/lib/fades.js
@@ -0,0 +1,19 @@
+function logoFadeIn(el) {
+    var windowHeight = $(window).height();
+    $(el).each(function(){
+        var thisPos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow + windowHeight - 200 > thisPos ) {
+            $(this).addClass("logo-fadein");
+        }
+    });
+}
+
+$(document).ready(function(){
+    logoFadeIn('.kvka-logo');
+});
+
+$(window).scroll(function() {
+    logoFadeIn('.kvka-logo');
+});
