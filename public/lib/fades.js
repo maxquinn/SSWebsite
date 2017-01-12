c0f254bfd318af22d80be0ad695ca638e58ff468
Max <max.t.quinn@gmail.com>
2017-01-13 00:38:08 +1300

    fixed sam artist page

--- public/lib/fades.js
+++ public/lib/fades.js
@@ -43,15 +43,82 @@ function isInViewport(element) {
         rect.right <= (window.innerWidth || html.clientWidth)
     );
 }
+var testEle;
+$(document).ready(function () {
+    num1 = document.getElementById('p1');
+    num2 = document.getElementById('p2');
+    num3 = document.getElementById('p3');
+    num4 = document.getElementById('p4');
+    num5 = document.getElementById('p5');
+    num6 = document.getElementById('p6');
+    num7 = document.getElementById('p7');
+    num8 = document.getElementById('p8');
+    num9 = document.getElementById('p9');
+    num10 = document.getElementById('p10');
+    num11 = document.getElementById('p11');
+    num12 = document.getElementById('p12');
+    num13 = document.getElementById('p13');
+    num14 = document.getElementById('p14');
 
-$(window).scroll(function () {
-    if (isInViewport(row1)) {
-        document.getElementById("sam-ultimate").style.backgroundImage = "url('../public/images/SAM GAT WEB IMAGE/1.jpg')";
-        document.getElementById("sam-ultimate").style.transition = "background 3s linear";
-
-    }
-    else {
-        document.getElementById("sam-ultimate").style.backgroundImage = "none";
-        document.getElementById("sam-ultimate").style.transition = "background 3s linear";
-    }
+    $(window).scroll(function () {
+        if (isInViewport(num1)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/1.jpg');
+        }
+        else if (isInViewport(num2)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/2.jpg');
+        }
+        else if (isInViewport(num3)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/3.jpg');
+        }
+        else if (isInViewport(num4)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/4.jpg');
+        }
+        else if (isInViewport(num5)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/5.jpg');
+        }
+        else if (isInViewport(num6)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/6.jpg');
+        }
+        else if (isInViewport(num7)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/7.png');
+        }
+        else if (isInViewport(num8)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/8.png');
+        }
+        else if (isInViewport(num9)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/9.png');
+        }
+        else if (isInViewport(num10)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/10.png');
+        }
+        else if (isInViewport(num11)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/11.png');
+        }
+        else if (isInViewport(num12)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/12.png');
+        }
+        else if (isInViewport(num13)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/13.jpg');
+        }
+        else if (isInViewport(num14)) {
+            $('#faded').fadeIn("slow");
+            $('#fadeImage').attr("src",'../public/images/SAM GAT WEB IMAGE/SAM CV.png');
+        }
+        else {
+            $('#faded').fadeOut('slow');
+        }
+    });
 });
